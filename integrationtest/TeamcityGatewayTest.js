1dc8630efbae269501bf75796f60d2a25b178eda
Chad Gorshing <gorshing@gmail.com>
2017-04-04 20:26:46 -0500

    Getting tests functioning

--- integrationtest/TeamcityGatewayTest.js
+++ integrationtest/TeamcityGatewayTest.js
@@ -1,32 +1,41 @@
 var assert = require('assert');
 var should = require('should');
+var nock = require('nock');
 
 var TeamCityGateway = require('./../lib/gateways/TeamCityGateway.js');
 var fs = require('fs');
 
 suite('TeamCityGateway', function() {
-	var config;
-	setup(function() {
-		// config = JSON.parse(fs.readFileSync('./conf/default/config.json', "utf8"));
-	});
+  //var config;
+  var teamcityMock;
 
-	suite('constructor', function() {
-		test('should_create_TeamCityGateway', function() {
-			var gateway = new TeamCityGateway("testServer");
-			assert.equal(gateway.server, "testServer");
-		});
-	});
+  beforeEach(function() {
+    nock('http://localhost')
+      .get('/guestAuth/app/rest/buildTypes/id:BranchingTest_Build/builds?locator=running:any,branch:(unspecified:any),lookupLimit:2')
+      .replyWithFile(200, __dirname + '/samples/teamcityApi/success.json');
+  });
 
-	suite('getBuildForId', function() {
-		test('should_get_all_builds_for_project_id', function(done) {
+  setup(function() {
+    //config = JSON.parse(fs.readFileSync('./lib/conf/config.json', "utf8"));
+  });
 
-			var gateway = new TeamCityGateway(config);
+  suite('constructor', function() {
+    test('should_create_TeamCityGateway', function(done) {
+      var gateway = new TeamCityGateway("testServer");
+      assert.equal(gateway.server, "testServer");
+      done();
+    });
+  });
 
-			gateway.getBuildsForProjectId('BranchingTest_Build', function(err,result) {
-				result.count.should.be.above(-1);
-			});
+  suite('getBuildsForProjectId', function() {
+    test('should_get_all_builds_for_project_id', function(done) {
 
-			done();
-		});
-	});
+      var gateway = new TeamCityGateway("localhost");
+
+      gateway.getBuildsForProjectId('BranchingTest_Build', function(err,result) {
+        result.count.should.be.above(-1);
+        done();
+      });
+    });
+  });
 });
--- integrationtest/TeamcityServiceTest.js
+++ integrationtest/TeamcityServiceTest.js
@@ -12,51 +12,46 @@ var TeamCityGatewayMock = require('./../lib/gateways/TeamCityGatewayMock.js');
 var fs = require('fs');
 
 suite('TeamcityService', function() {
-	var config;
-	var service;
-	var mockGateway;
+  var config;
+  var service;
+  var mockGateway;
 
-	setup(function() {
-		// config = JSON.parse(fs.readFileSync('./config.json', "utf8"));
-		config = require('nconf').get();
+  setup(function() {
+    // config = JSON.parse(fs.readFileSync('./config.json', "utf8"));
+    config = require('nconf').get();
     mockGateway = new TeamCityGatewayMock();
-		service = new TeamCityService(config, mockGateway);
+    service = new TeamCityService(config, mockGateway);
 
-	});
+  });
 
-	suite('constructor', function() {
-		test('should_create_TeamCityService', function() {
-      //service.should.be.a('object').and.have.property('gateway', mockGateway);
-      //service.should.be.a('object');
+  suite('constructor', function() {
+    test('should_create_TeamCityService', function() {
+      service.should.be.an.instanceOf(Object).and.have.property('gateway', mockGateway);
+    });
+  });
 
-      console.log("****************************************************************");
-      console.log(service.gateway == mockGateway);
-      //service.should.have.property('gateway', mockGateway);
-		});
-	});
+  suite('getBuildActivityForBuildId', function() {
+    test('should_return_build_activity', function(done) {
 
-	suite('getBuildActivityForBuildId', function() {
-		test('should_return_build_activity', function(done) {
+      service.getBuildActivityForBuildId('BranchingTest_Build', function(err, result) {
+        // console.log(prettyjson.render(result));
+        result.should.be.an.instanceof(BuildActivity, "BuildActivity");
+        result.currentBuild.buildTypeId.should.equal('BranchingTest_Build');
+        done();
+      });
+    });
+  });
 
-			service.getBuildActivityForBuildId('BranchingTest_Build', function(err, result) {
-				// console.log(prettyjson.render(result));
-				result.should.be.an.instanceof(BuildActivity, "BuildActivity");
-				result.currentBuild.buildTypeId.should.equal('BranchingTest_Build');
-				done();
-			});
-		});
-	});
+  suite('getAllBuilds', function() {
+    test('should_return_correct_number_of_BuildActivity_objects', function(done) {
 
-	suite('getAllBuilds', function() {
-		test('should_return_correct_number_of_BuildActivity_objects', function(done) {
-
-			service.getAllBuilds(function(err, result) {
-				should.not.exist(err);
-				should.exist(result);
-				result.should.be.an.instanceOf(Array);
-				result.length.should.be.within(0,2);
-				done();
-			});
-		});
-	});
+      service.getAllBuilds(function(err, result) {
+        should.not.exist(err);
+        should.exist(result);
+        result.should.be.an.instanceOf(Array);
+        result.length.should.be.within(0,2);
+        done();
+      });
+    });
+  });
 });
